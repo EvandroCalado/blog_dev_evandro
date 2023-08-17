5895ee68ea9db9f670b3fd4bc00f9b623d21e9ae
evandrocalado <evandrocalado07@gmail.com>
2023-08-17 10:27:37 -0300

    create generateError and generateSuccess

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from 'next/server';
 import prisma from '../../prisma';
 
 export const connectToDb = async () => {
@@ -6,4 +7,18 @@ export const connectToDb = async () => {
   } catch (error: any) {
     throw new Error(error);
   }
+};
+
+export const generateErrorMessage = (data: any, status: number) => {
+  return NextResponse.json(
+    { message: 'Erro', ...data },
+    { status, statusText: 'OK' },
+  );
+};
+
+export const generateSuccessMessage = (data: any, status: number) => {
+  return NextResponse.json(
+    { message: 'Sucesso', ...data },
+    { status, statusText: 'ERROR' },
+  );
 };

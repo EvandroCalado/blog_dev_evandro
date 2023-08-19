2cc5d3026e3fb1a52b420dd8577fdac9c9c965a5
evandrocalado <evandrocalado07@gmail.com>
2023-08-18 23:20:34 -0300

    add HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HomePage from '../components/HomePage';
+
 export default function Home() {
   return (
-    <main>
-      <h1 className="flex text-3xl font-bold underline">Home</h1>
-    </main>
+    <>
+      <HomePage />
+    </>
   );
 }

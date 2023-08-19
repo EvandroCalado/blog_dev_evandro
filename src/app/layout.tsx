e0bb3b5c515f34217d03490ae34923f6421b69b8
evandrocalado <evandrocalado07@gmail.com>
2023-08-18 23:20:14 -0300

    add Header, Footer and setup layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import Providers from '../utils/providers';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export const metadata: Metadata = {
   title: 'Blog Dev Evandro',
@@ -16,7 +18,9 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className="flex min-h-screen flex-col">
         <Providers>
+          <Header />
           <main className="flex-1">{children}</main>
+          <Footer />
         </Providers>
       </body>
     </html>
